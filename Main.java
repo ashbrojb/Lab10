3e9164465248afce5b6714b353a364ec17b381b5
ashbrojb <>
2021-04-12 15:36:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* My partner for this lab was Christopher Fenter*/
+
+class Main {
+  public static void main(String[] args) {
+    Sugar tree = new Sugar("tree");
+    tree.cutOut(tree.getShape(), 10);
+    tree.setBake(350, 12);
+    tree.decorateCookie(tree.getIsReady());
+
+    Sugar aCookie = new Sugar();
+    aCookie.decorateCookie(aCookie.getIsReady());
+  }
+}
